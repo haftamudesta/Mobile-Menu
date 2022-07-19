bd5af91c7bbe85e6d6d7278195f396b0542b3167
haftamudesta <desta451616@hotmail.com>
2022-07-20 01:34:54 +0300

    Modify mobile menu

--- README.md
+++ README.md
@@ -1,27 +1,24 @@
-
 # Project Name:portfolio
-This is my first project at Microverse.
-- [link is here](https://haftamudesta.github.io/desta451616-hotmail.com.github.io/portfolio.html)
+This is my portfolio project at Microverse.
 ## Built With
-- HTML
-- CSS
+.HTML
+.CSS
 ## Getting Started
 to implement and run this project you should have the following prerequisites  
 ## Prerequisites
-- Text editor,
-- browser like crome.
-> If you have the above mentioned  prerequistes you can download the project to your local machine from text editor by using the following steps:
- - Clone Git repository: URL (https://github.com/haftamudesta/portfolio) 
- - Open in your local machine
+Text editor
+browser like crome
 ## Author
- - [Githab](https://github.com/settings/profile)
-- [linkedin](https://www.linkedin.com/in/haftamu-desta-795791a1/)
-- [tiwtter](https://twitter.com/DestaHftamu?t=NQ4ovkdWbsfsjh62NFEXFg&s=09)
+## Name: Haftamu Desta
+### Here are my social media links
+- [haftamudesta](https://github.com/haftamudesta)
+- [Haftamu Desta](https://www.linkedin.com/in/haftamu-desta-795791a1/)
+- [DestaHaftamu](https://twitter.com/DestaHftamu?t=NQ4ovkdWbsfsjh62NFEXFg&s=09)
 
 ## Contributing
 Contributions, issues, and feature requests are welcome!
 ## Show your support
-give tumbs up if you like this project.
+give tumps if you like this project.
 ## Acknowledgments
 Microverse
 ## License
--- portfolio.js
+++ portfolio.js
@@ -1,34 +1,34 @@
 const hambergur = document.querySelector('.allign');
-const contacts = document.querySelector('.contacts');
-const closes = document.querySelector('.close');
+const contactsMenu = document.querySelector('.contacts-menu');
+const closes = document.querySelector('.close-menu');
 const main = document.querySelector('.main');
-const navlink = document.querySelector('.navlinks1');
+const navlink = document.querySelectorAll('.navlinks');
+const navContainer = document.querySelector('.nav-container');
 hambergur.addEventListener('click', (e) => {
-  e.preventDefault();
   hambergur.classList.toggle('close');
   closes.style.display = 'block';
-  hambergur.style.display = 'none';
-  contacts.style.display = 'block';
+  contactsMenu.classList.toggle('active');
+  navContainer.style.height = '300px';
   main.style.opacity = '0';
+  hambergur.style.display = 'none';
 });
-
 closes.addEventListener('click', (e) => {
-  e.preventDefault();
   closes.classList.toggle('allign');
   hambergur.style.display = 'block';
   closes.style.display = 'none';
-  contacts.style.display = 'none';
+  navContainer.style.height = '92px';
+  contactsMenu.style.display = 'none';
   main.style.opacity = '1';
 });
-navlink.addEventListener('click', (e) => {
-  e.preventDefault();
-  hambergur.style.display = 'block';
-  closes.style.display = 'none';
-  contacts.style.display = 'none';
-  main.style.opacity = '1';
+navlink.forEach(n => {
+  n.addEventListener('click', () => {
+    hambergur.style.display = 'block';
+    closes.style.display = 'none';
+    main.style.opacity = '1';
+    contactsMenu.style.display = 'none';
+  }
+  )
 });
-contacts.addEventListener('click', (e) => {
-  e.preventDefault();
-  contacts.style.display = 'block';
-  main.style.opacity = '1';
-});
+
+
+
